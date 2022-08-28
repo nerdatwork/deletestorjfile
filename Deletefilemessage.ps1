2413729ff0bf5c11c3926f07e00143692a13cf5d
nerdatwork <15882196+nerdatwork@users.noreply.github.com>
2022-08-29 00:24:20 +0530

    Create files using CLI
     Format of command: <scriptname> <path to storj> <path to log file>

--- Deletefilemessage.ps1
+++ Deletefilemessage.ps1
@@ -7,9 +7,6 @@ Write-Host "`t###############################################################" -
 Write-Host "`t     Script to stop `"unable to delete piece`" error message "
 Write-Host "`t###############################################################" -ForegroundColor Cyan
 
-Write-Host "Enter Storj path containing `"Storage`" folder : " -ForegroundColor Magenta -NoNewline
-$storjPath = Read-Host 
-
 $satIDHT = @{
 	"12EayRS2V1kEsWESU9QMRseFhdxYxKicsiFmxrsLZHeLUtdps3S" = "ukfu6bhbboxilvt7jrwlqk7y2tapb5d2r2tsmj2sjxvw5qaaaaaa"
 	"12L9ZFwhzVpuEKMUNUqkaTLGzwY9G24tbiigLiXpmZWKwmcNDDs" = "v4weeab67sbgvnbwd5z7tweqsqqun7qox2agpbxy44mqqaaaaaaa"
@@ -20,38 +17,35 @@ $satIDHT = @{
 	"118UWpMCHzs6CvSgWd9BfFVjw5K9pZbJjkfZJexMtSkmKxvvAW"  = "abforhuxbzyd35blusvrifvdwmfx4hmocsva4vmpp3rgqaaaaaaa"
 }
 
-do{
-	Write-Host "`tEnter Satellite ID or q to quit : " -ForegroundColor Magenta -NoNewline
-	$satID = Read-Host 
-	if ($satID -eq "q")	{
-		Write-Host "`n`tNerdAtWork thanks you for using the script." -ForegroundColor Green
-		pause
-		break
+# Format of command: <scriptname> <path to storj> <path to log file>
+
+	if ($args.Count -eq 2){
+		$storjPath = $args[0]
+		$logfile = $args[1]
 	}
-	
-	$satFolder = $satIDHT[$satID]
-	
-	$path = $storjPath + "\storage\blobs\" + $satFolder
-	# Get inside Satellite Folder
-	Set-Location -Path $path 
-	do{
-		Write-Host "`t`tEnter Piece ID or q to quit : " -ForegroundColor Yellow -NoNewline
-		$pieceID = Read-Host 
-		if ($pieceID -eq 'q'){
-			# Setting path to main folder to traverse for new Satellite ID
-			Set-Location -Path $storjPath
-			break
-		}
-		$pieceFolder = $pieceID.Substring(0, 2)
-		# Get inside piece folder to create file
-		Set-Location -Path ($path + "\" + $pieceFolder) 
-		
-		$fileName = $pieceID.Substring(2, $pieceID.Length - 2) + ".sj1"
-		# Create file & suppress creation details
-		if (New-Item  $fileName){		
-			Write-Host "`t`tFile was successfully created!" -ForegroundColor Green
-		}
+	else{
+		Write-Host "Enter Storj path containing `"Storage`" folder : " -ForegroundColor Magenta -NoNewline
+			$storjPath = Read-Host
+
+		Write-Host "Enter path containing `"log`" file : " -ForegroundColor Magenta -NoNewline
+			$logfile = Read-Host
 	}
-	while ($true)
-}
-while ($true)
+		foreach ($logline in Get-Content $logfile ){
+			$logarr = $logline.Split('"')
+			$satID = $logarr[7]
+			$pieceID = $logarr[11]
+				
+			$satFolder = $satIDHT[$satID]
+				
+			$pieceFolder = $pieceID.Substring(0, 2)
+			$fileName = $pieceID.Substring(2, $pieceID.Length - 2) + ".sj1"
+			Set-Location -Path ($storjPath + "\storage\blobs\" + $satFolder + "\" + $pieceFolder)
+				
+			if (New-Item  $fileName){
+				Write-Host "`n`t" $pieceID -ForegroundColor Yellow -NoNewline
+				Write-Host " was successfully FIXED!" -ForegroundColor Green
+			}
+		}	
+
+ Pause
+
